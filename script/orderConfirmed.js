25adbef2439dc81f727c5d446cd69baf1deb7946
Pawan Singh <113767530+almightycoder4@users.noreply.github.com>
2022-12-07 13:26:51 +0530

    Delete script directory

--- script/orderConfirmed.js
+++
@@ -1,8 +0,0 @@
-
-import { navbar } from "./module/nav.js";
-let header = document.getElementById("header");
-header.innerHTML = navbar();
-
-import { footer } from "./module/footer.js";
-let footerdisplay = document.getElementById("footerdiv");
-footerdisplay.innerHTML = footer();
